148c674f03ea989ab60e8c3c5bacf337f340fc8c
DIMOOON <68978608+reewardius@users.noreply.github.com>
2024-07-28 01:49:56 +0300

    Update run_scanners.sh

--- run_scanners.sh
+++ run_scanners.sh
@@ -11,16 +11,20 @@ mkdir -p $RESULTS_DIR
 # Запускаем Trivy
 echo "Running Trivy..."
 trivy plugin install github.com/fatihtokus/scan2html
-trivy scan2html fs --vuln-type library --scanners vuln,misconfig $CODE_DIR $RESULTS_DIR/trivy.html
+trivy scan2html fs --cache-dir /tmp/trivy/ --vuln-type library --scanners vuln,misconfig $CODE_DIR $RESULTS_DIR/trivy.html
 
 # Запускаем Checkov
 echo "Running Checkov..."
-checkov -d $CODE_DIR > $RESULTS_DIR/checkov.results
+checkov -d $CODE_DIR --output github_failed_only > $RESULTS_DIR/checkov.txt
 
 # Запускаем TruffleHog
 echo "Running TruffleHog..."
-trufflehog filesystem $CODE_DIR --only-verified > $RESULTS_DIR/trufflehog.results
+trufflehog filesystem $CODE_DIR --only-verified > $RESULTS_DIR/trufflehog.txt
 
 # Запускаем Semgrep
 echo "Running Semgrep..."
-semgrep scan $CODE_DIR --config auto --text > $RESULTS_DIR/semgrep.results
+semgrep scan $CODE_DIR --config auto --json > $RESULTS_DIR/semgrep.json
+
+curl https://raw.githubusercontent.com/reewardius/RASM/main/semgrep_friendly.py -o semgrep_friendly.py
+python semgrep_friendly.py -f $RESULTS_DIR/semgrep.json -o $RESULTS_DIR/semgrep_report.md
+rm $RESULTS_DIR/semgrep.json
